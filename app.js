fac445496acc0891690ad46388eef06cbc65f3c2
Ilyes Djari <ilyes.djari@student.ehb.be>
2021-11-27 01:09:48 +0100

    Cleaned my front end JS
    Because all my calls were done in JS a lot of this code was remaining. I just left the interesting code i need when i'll receive the data from the back end.

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-var redirect_uri = "http://127.0.0.1:5500/pages/home.html";
-var client_id = "75d6012515364a608ebbf7ec5113308c"; 
-var client_secret = "e9069eeeb800474394cbe578f1a93c67"; 
-var access_token = null;
-var refresh_token = null;
-
-const AUTHORIZE = "https://accounts.spotify.com/authorize";
-const TOKEN = "https://accounts.spotify.com/api/token";
-const PLAYER = "https://api.spotify.com/v1/me/player";
-const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
-const PROFILE = "https://api.spotify.com/v1/me";
-const CHOICES = [];
-
 document.documentElement.addEventListener("load", function(){
     document.getElementById("loader").style.display = "block";
 });
@@ -20,48 +7,10 @@ window.addEventListener("load", function(){
 });
 
 
-// Login in on Spotify with Back-end 
+// Redirection to Spotify login with Back-end 
 function requestAuthorization() {
     location.replace("http://localhost:3000/login");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function onPageLoad(){
-    client_id = localStorage.getItem("client_id");
-    client_secret = localStorage.getItem("client_secret");
-
-    if ( window.location.search.length > 0 ){
-        handleRedirect();
-    }
-    else{
-        access_token = localStorage.getItem("access_token");
-        if ( access_token == null ){
-            document.getElementById("tokenSection").style.display = 'block';  
-        }
-        else { 
-            userInformation();
-            currentlyPlaying();
-            newRelease();
-        }
-    }
-}
-
 
 
 
@@ -75,21 +24,13 @@ function newRelease(){
     callApi( "GET", RELEASE, null, newRealeaseResponse);
 }
 
-function currentlyPlaying(){
-     callApi( "GET", PLAYER, null, handleCurrentlyPlayingResponse);
-     
-}
 
-function userInformation() {
-    callApi( "GET", PROFILE, null, userResponse);
-}
 
 function userResponse() {
     var data = JSON.parse(this.responseText);
     const time = new Date().getHours();
-    if ( data != null ){
-            document.getElementById("username").innerHTML = data.display_name; 
-            document.getElementById("userpicture").src = data.images[0].url;
+            //document.getElementById("username").innerHTML = data.display_name; 
+            //document.getElementById("userpicture").src = data.images[0].url;
 
             if (time < 12) {
                 document.getElementById("daytime").innerHTML = "Good morning, ";
@@ -100,32 +41,22 @@ function userResponse() {
             else {
                 document.getElementById("daytime").innerHTML = "Good evening, ";
             }
-    }
-    else if ( this.status == 401 ){
-    refreshAccessToken();
-    }
 }
 
 
 function handleCurrentlyPlayingResponse(){
     var data = JSON.parse(this.responseText);
-    if ( this.status == 200 ){
         if ( data.item != null ){       
             document.getElementById("imgcurrent").src = data.item.album.images[0].url;
             document.getElementById("artistcurrent").innerHTML = data.item.artists[0].name;
             document.getElementById("songcurrent").innerHTML = data.item.name;
            //setTimeout(currentlyPlaying, 1000); 
         }
-    }
-    else if ( this.status == 401 ){
-        refreshAccessToken();
-    }
 }
 
 
 function newRealeaseResponse(){
     var data = JSON.parse(this.responseText);
-    if ( data.albums != null ){
         for(let i = 0; i<5; i++) {
             document.getElementById("release").insertAdjacentHTML('afterbegin', `
             <a href="${data.albums.items[i].external_urls.spotify}"">
@@ -136,10 +67,6 @@ function newRealeaseResponse(){
             </div>
             </a>`);
         }
-    }
-    else if ( this.status == 401 ){
-    refreshAccessToken();
-    }
 }
 
 
